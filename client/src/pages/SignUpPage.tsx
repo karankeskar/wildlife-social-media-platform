59845b5af0b8372c6e947b4f6d6f7daddb8d7cc5
Karan D K <40066189+karankeskar@users.noreply.github.com>
2025-11-29 16:58:41 +1100

    Chore:Setup Sign-Up and Shadcn

---
+++ client/src/pages/SignUpPage.tsx
@@ -0,0 +1,10 @@
+import { SignupForm } from "@/components/signup-form";
+export default function SignupPage() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-background">
+      <div className="w-full max-w-md px-4">
+        <SignupForm></SignupForm>
+      </div>
+    </div>
+  );
+}
